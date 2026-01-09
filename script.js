313cc9afbd0f37db7d4a7400ddf7d15049c1cb13
PABOLU DURGA PRASAD <paboludurga2004@gmail.com>
2026-01-09 16:11:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const noWhiteSpace = document.getElementById("noWhiteSpace").innerHTML;
 console.log(noWhiteSpace);
 
-const whitespace = document.getElementById("whitespace").innerHTML;
-console.log(whitespace);
+const WhiteSpace = document.getElementById("whitespace").innerHTML;
+console.log(WhiteSpace);
